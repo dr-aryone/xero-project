cfc64df2aa53aa649817e85a17a12a3385d7e97b
Helen Anderson <helen.anderson26@gmail.com>
2017-05-01 16:47:06 +0100

    updated server.js

--- server.js
+++ server.js
@@ -4,10 +4,9 @@ var app = express();
 // var mongoose = require('mongoose'); // DB control program
 // var xeroClient = require('./xero-node-client');
 
-var  xero = require('xero-node');
+var xero = require('xero-node');
 var fs = require('fs');
 var config = require("./config.json");
-
 
 //Private key can either be a path or a String so check both variables and make sure the path has been parsed.
 if (config.privateKeyPath && !config.privateKey) {
@@ -17,28 +16,70 @@ if (config.privateKeyPath && !config.privateKey) {
 console.log("config", config);
 
 const xeroClient = new xero.PrivateApplication(config);
+xero.setLogLevel("debug");
+
 
 console.log(xeroClient, typeof xeroClient);
 app.use(express.static('public'));
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 
 // parse application/json
 app.use(bodyParser.json());
 
-app.get('/xero', function(req, res){
-  console.log('in route', xeroClient.core.invoices.getInvoices);
-  var filter = 'Status == "AUTHORISED"';
-  // Print out a count of invoices
-  xeroClient.core.invoices.getInvoices({where: filter})
-  .then(function(invoices) {
-      console.log("Invoices: " + invoices.length);
-  }).catch(function(err) {
-      console.log(err);
-  });
+
+// Print out a count of invoices
+app.get('/xero', function(req, res) {
+    console.log('in route', xeroClient.core.invoices.getInvoices);
+    var filter = 'Status == "AUTHORISED"';
+
+    xeroClient.core.invoices.getInvoices({
+            where: filter
+        })
+        .then(function(invoices) {
+            console.log("Invoices: " + invoices.length);
+            var response = {
+                length: invoices.length
+            }
+            res.json(response);
+        }).catch(function(err) {
+            console.log(err);
+        });
 });
 
-app.listen(3333, function(){
+
+//Print the name of a contact
+xeroClient.core.contacts.getContacts({
+        where: 'Name.Contains("Bayside")'
+    })
+    .then(function(contacts) {
+            contacts.forEach(function(contact) {
+                    console.log(contact.Name);
+                 var contactResponse = {
+                     contact.Name
+                 }
+                res.json(contactResponse);
+            }).catch(function(err) {
+            console.log(err);
+        });
+    });
+
+
+/*filter contacts that are type Customer
+var filter = 'IsCustomer == true';
+
+xeroClient.core.contacts.getContacts({where: filter})
+   .then(function(contacts) {
+      //We've got some contacts
+      contacts.forEach(function(contact){
+         //do something useful
+         console.log(contact.IsCustomer); //will be true
+      });
+   }) */
+
+app.listen(3333, function() {
     console.log('Server listening');
 });
